80d8e19d6dd25c4409191e8301af1345c664e550
Rawah Abdulsamad <rawahprogrammer.gmail.com>
2025-10-03 19:04:45 +0300

    footer page of store

---
+++ template/footer.php
@@ -0,0 +1,14 @@
+<footer>
+   ©
+   <script>
+      document.write(new Date().getFullYear())
+   </script>
+   TechStore All rights reserved </p>
+</footer>
+
+<!-- Script -->
+<script src="./index.js"></script>
+</div>
+</body>
+
+</html>
