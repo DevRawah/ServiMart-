3714267ccf02bf89816c7848badd1a1600ec87f0
Rawah Abdulsamad <rawahprogrammer.gmail.com>
2025-10-03 19:25:12 +0300

    controle panel footer of store

---
+++ admin/template/footer.php
@@ -0,0 +1,36 @@
+</div>
+</div>
+<footer class="footer">
+    <div class="container-fl uid">
+
+        <p class="copyright text-center">
+            ©
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            <a href="#">Creative Tim</a>, made with love for a better web
+        </p>
+
+    </div>
+</footer>
+
+
+</body>
+<!--   Core JS Files   -->
+<script src="<?php echo $setting['admin_assets'] ?>/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="<?php echo $setting['admin_assets'] ?>/js/core/popper.min.js" type="text/javascript"></script>
+<script src="<?php echo $setting['admin_assets'] ?>/js/core/bootstrap.min.js" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="<?php echo $setting['admin_assets'] ?>/js/plugins/bootstrap-switch.js"></script>
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!--  Chartist Plugin  -->
+<script src="<?php echo $setting['admin_assets'] ?>/js/plugins/chartist.min.js"></script>
+<!--  Notifications Plugin    -->
+<script src="<?php echo $setting['admin_assets'] ?>/js/plugins/bootstrap-notify.js"></script>
+<!-- Control Center for Light Bootstrap Dashboard: scripts for the example pages etc -->
+<script src="<?php echo $setting['admin_assets'] ?>/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
+<!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
+<script src="<?php echo $setting['admin_assets'] ?>/js/demo.js"></script>
+
+</html>
